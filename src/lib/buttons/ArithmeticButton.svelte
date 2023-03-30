013c8960292bee1b6c3df05fee6655cc9eae49a7
Say Peng <yyueniao0913@gmail.com>
2023-03-31 06:29:06 +0800

    style(ArithmeticButton): change background color

--- src/lib/buttons/ArithmeticButton.svelte
+++ src/lib/buttons/ArithmeticButton.svelte
@@ -3,3 +3,17 @@
 </script>
 
 <button {disabled} on:click><slot /></button>
+
+<style>
+	button {
+		background-color: #3d3dc6;
+	}
+
+	button:hover {
+		background-color: #6b6bcb;
+	}
+
+	button:disabled {
+		background-color: #d6d6d6;
+	}
+</style>
